67e4667302ba46763c97abf705d8f676d81bdaff
rmdocherty <rmdocherty2@gmai.com>
2024-01-10 16:52:44 +0000

    added scraper code - uses requests to parse xml data from api related to query and saves to dataframe

---
+++ scrape.py
@@ -0,0 +1,116 @@
+import requests
+import pandas as pd
+from time import time
+import xml.etree.ElementTree as ET
+from typing import Tuple, List
+
+CSV_PATH = "sample_data/paper_data.csv"
+SEP = "«"
+MAX_RESULTS = 300
+# up to 4 requests per second with 1s sleep
+REQUEST_DELAY_S: float = 3
+N_RETRIES: int = 3
+
+"""
+url = "http://export.arxiv.org/api/query?search_query=all:checkerboard&start=0&max_results=100&sortBy=lastUpdatedDate&sortOrder=descending"
+
+results = requests.get(
+    url,
+)  # headers=arxiv_api_headers
+
+with open("test_scrape.xml", "w+") as f:
+    f.write(results.text)
+"""
+
+
+def create_csv() -> pd.DataFrame:
+    df = pd.DataFrame(columns=["url", "summary", "date"])
+    df.to_csv(CSV_PATH, sep=SEP)
+    return df
+
+
+def load_csv() -> pd.DataFrame:
+    return pd.read_csv(CSV_PATH, sep=SEP)
+
+
+def make_api_request(
+    query_term: str,
+    start: int = 0,
+    max_results: int = 100,
+    sort_by: str = "lastUpdatedDate",
+    sort_order="descending",
+) -> str:
+    url = f"http://export.arxiv.org/api/query?search_query={query_term}&start={start}&max_results={max_results}&sortBy={sort_by}&sortOrder={sort_order}"
+    result = requests.get(url)
+    return result.text
+
+
+def handle_entry(entry_elem) -> Tuple[str, str, str]:
+    id, summary, published = "", "", ""
+    for child in entry_elem:
+        if "id" in child.tag:
+            id = child.text
+        elif "summary" in child.tag:
+            summary = child.text
+        elif "published" in child.tag:
+            published = child.text
+    return id, summary, published
+
+
+def handle_xml(xml_string: str) -> Tuple[List[str], List[str], List[str]]:
+    root = ET.fromstring(xml_string)
+    ids: List[str] = []
+    summaries: List[str] = []
+    dates: List[str] = []
+    for child in root:
+        if "entry" in child.tag:
+            id, summary, date = handle_entry(child)
+            ids.append(id)
+            summaries.append(summary)
+            dates.append(date)
+    return ids, summaries, dates
+
+
+def add_to_csv(
+    ids: List[str], summaries: List[str], dates: List[str], df: pd.DataFrame
+) -> pd.DataFrame:
+    new_df = pd.DataFrame(data={"url": ids, "summary": summaries, "date": dates})
+    sum_df = pd.concat([df, new_df])
+    return sum_df
+
+
+def save_df(df: pd.DataFrame) -> None:
+    df.to_csv(CSV_PATH, sep=SEP)
+
+
+def paper_info_loop() -> None:
+    df = create_csv()
+
+    prev_time = time()
+    n_papers = 0
+    max_papers = 1000
+
+    while n_papers < max_papers:
+        new_time = time()
+        if (new_time - prev_time) > REQUEST_DELAY_S:
+            xml = make_api_request("all:microscopy", n_papers, MAX_RESULTS)
+            ids, summaries, dates = handle_xml(xml)
+
+            if len(ids) > 1:
+                df = add_to_csv(ids, summaries, dates, df)
+                save_df(df)
+                n_papers += len(ids)
+                print(f"Successful scrape of {len(ids)} papers. \nTotal N={n_papers}")
+            prev_time = new_time
+
+
+def download_pdf(url: str, save_path: str) -> None:
+    id = url.split("/")[-1]
+    pdf_url = f"http://export.arxiv.org/pdf/{id}.pdf"
+    result = requests.get(pdf_url)
+    with open(save_path, "wb+") as f:a
+        f.write(result.content)
+
+
+if __name__ == "__main__":
+    download_pdf("http://arxiv.org/abs/2210.02100v3", "foo.pdf")
