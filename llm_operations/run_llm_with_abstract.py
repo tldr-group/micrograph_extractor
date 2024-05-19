071c54e7c5e0a32eb02c512de7232c13f9194007
rmdocherty <rmdocherty2@gmai.com>
2024-05-19 17:44:34 +1200

    formatting to remove code that runs on import

--- llm_operations/run_llm_with_abstract.py
+++ llm_operations/run_llm_with_abstract.py
@@ -9,25 +9,27 @@ from gpt_utils import *
 openai.api_key = os.getenv("OPENAI_API_KEY")
 client = openai.OpenAI(api_key=openai.api_key)
 
+
 def assistant(abstract, captions):
     """
     Given an abstract and a caption, generate a JSON output with the following fields:
     1. IsMicrographPresent: Whether a micrograph is present in the figure.
     2. MicroscopyTechniqueUsed: The microscopy technique used to produce the micrograph.
     3. MaterialShown: The material shown in the micrograph."""
-    
-    def get_completion(messages, model="gpt-4-0125-preview", 
-                        temperature=0, max_tokens=500):
 
-            completion = client.chat.completions.create(
-                model= model,
-                messages=messages,
-                temperature=temperature,
-                max_tokens=max_tokens
-            )
-            return completion.choices[0].message.content
+    def get_completion(
+        messages, model="gpt-4-0125-preview", temperature=0, max_tokens=500
+    ):
 
-    system_message ="""
+        completion = client.chat.completions.create(
+            model=model,
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens,
+        )
+        return completion.choices[0].message.content
+
+    system_message = """
     You are an expert materials scientist. You study micrographs, which are images taken using a microscope. 
 
     Based on an academic paper's abstract and a specific figure's caption, provide answers in JSON format to the following questions:
@@ -59,20 +61,20 @@ def assistant(abstract, captions):
     captions_escaped = repr(captions)
     user_message = f""" The abstract is: {abstract_escaped}, and the captions are: {captions_escaped}"""
 
-    messages =  [  
-    {'role':'system', 
-    'content': system_message},    
-    {'role':'user', 
-    'content': f"{user_message}"},  
-    ] 
+    messages = [
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": f"{user_message}"},
+    ]
 
     response = get_completion(messages)
     return response
 
 
 def process_folder(base_path):
-    error_log_path = os.path.join(base_path, 'error_log.txt')
-    folders = [f for f in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, f))]
+    error_log_path = os.path.join(base_path, "error_log.txt")
+    folders = [
+        f for f in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, f))
+    ]
     total_folders = len(folders)
 
     for i, folder in enumerate(folders):
@@ -81,47 +83,47 @@ def process_folder(base_path):
             print(f"Start processing: {folder}")
             try:
                 # Process the folder contents
-                with open(os.path.join(folder_path, 'captions.json'), 'r') as file:
+                with open(os.path.join(folder_path, "captions.json"), "r") as file:
                     captions_data = json.load(file)
 
-                with open(os.path.join(folder_path, 'paper_data.json'), 'r') as file:
+                with open(os.path.join(folder_path, "paper_data.json"), "r") as file:
                     paper_data = json.load(file)
 
-                abstract = paper_data.get('abstract', '')
+                abstract = paper_data.get("abstract", "")
                 llm_label_data = []
 
                 for item in captions_data:
-                    caption = item['caption']
-                    name = item['name']
-                    figure_type = item['figType']
+                    caption = item["caption"]
+                    name = item["name"]
+                    figure_type = item["figType"]
 
-                    if figure_type == 'Figure':
+                    if figure_type == "Figure":
                         response = assistant(abstract, caption)
                         response_data = extract_json_from_response(response)
-                        response_data['figure'] = name
+                        response_data["figure"] = name
                         llm_label_data.append(response_data)
 
-                with open(os.path.join(folder_path, 'labels_gpt4_with_abstract.json'), 'w') as file:
+                with open(
+                    os.path.join(folder_path, "labels_gpt4_with_abstract.json"), "w"
+                ) as file:
                     json.dump(llm_label_data, file, indent=4)
                 print(f"Finished processing the folder: {folder}")
 
             except Exception as e:
                 # Write the error to the error log
-                with open(error_log_path, 'a') as error_file:
+                with open(error_log_path, "a") as error_file:
                     error_message = f"Error processing folder {folder}: {e}\n"
                     error_file.write(error_message)
-                print(f"An error occurred while processing the folder {folder}. Please check the error log for details.")
-            
+                print(
+                    f"An error occurred while processing the folder {folder}. Please check the error log for details."
+                )
+
             # Print progress
             progress_percentage = (i + 1) / total_folders * 100
             print(f"Processing... {progress_percentage:.2f}% complete")
 
+
 # Process each DOI-named folder in the 'train' directory
-train_directory_path = './micrograph_dataset_new/train'
-process_folder(train_directory_path)
-                
-
-
-
-
-
+if __name__ == "__main__":
+    train_directory_path = "./micrograph_dataset_new/train"
+    process_folder(train_directory_path)
