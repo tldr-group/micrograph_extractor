b18eb2b44d3a8e0dd9c71c1c0bc6b0ee3e7132d7
rmdocherty <rmdocherty2@gmai.com>
2024-05-24 16:03:31 +1200

    updated

--- build_scripts/build_extra.sh
+++ build_scripts/build_extra.sh
@@ -2,10 +2,11 @@
 
 # clone MatSciBERT
 git clone https://github.com/M3RG-IITD/MatSciBERT
+mv MatSciBERT/ plots/MatSciBERT/
 
 # assumes you've created extractor already with build.sh
 conda activate extractor
 
 conda install -y pytorch==1.7.0 cudatoolkit=10.1 -c pytorch
 # assumes cloned to MatSciBERT/  
-conda install --yes --file MatSciBERT/requirements.txt
+conda install --yes --file plots/MatSciBERT/requirements.txt
